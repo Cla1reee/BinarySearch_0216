f4a77abc9188c991fbf5b58cbb502e6cac7fa539
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-04-21 14:20:27 +0700

    Enhance binary search function to display found element index and improve output formatting

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -51,15 +51,39 @@ void display() {
     cout << endl;
 }
 
-void binarySearch() {
+void binarySearch()
+{
     char ulang;
-    do {
-        cout << "\n-------------------------------\n";
+    do
+    {
+        cout << "\n===============================\n";
         cout << "     Pencarian Binary Search    \n";
-        cout << "-------------------------------\n";
+        cout << "===============================\n";
         cout << "Masukkan elemen yang ingin dicari: ";
         cin >> x;
-        
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[v] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
+                return;
+            }
+
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
         
     } while (ulang == 'y' || ulang == 'Y');
 }
